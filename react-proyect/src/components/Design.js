f6fd710ba7ad15a12777a2f306e6ae820727c30f
silviaespanagil <silviaespanagil@gmail.com>
2021-06-02 13:06:48 +0200

    Add Palettes Component

--- react-proyect/src/components/Design.js
+++ react-proyect/src/components/Design.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Palettes from "./Palettes.js";
 import "../stylesheets/App.scss";
 
 class Design extends React.Component {
@@ -17,46 +18,7 @@ class Design extends React.Component {
           </div>
           <div className="section__design--box js-collapsible collapsible--close">
             <h3 className="section__design--subtitle">Colores</h3>
-            <label className="section__desing--boxcolor" for="colorpalette1">
-              <input
-                className="section__design--check js-colorPalette1"
-                type="radio"
-                value="g"
-                name="design-color"
-                checked
-              />
-              <ul className="section__design--palette">
-                <li className="box color1-1"></li>
-                <li className="box color1-2"></li>
-                <li className="box color1-3"></li>
-              </ul>
-            </label>
-            <label className="section__desing--boxcolor" for="colorpalette2">
-              <input
-                className="section__design--check js-colorPalette2"
-                type="radio"
-                value="r"
-                name="design-color"
-              />
-              <ul className="section__design--palette">
-                <li className="box color2-1"></li>
-                <li className="box color2-2"></li>
-                <li className="box color2-3"></li>
-              </ul>
-            </label>
-            <label className="section__desing--boxcolor" for="colorpalette3">
-              <input
-                className="section__design--check js-colorPalette3"
-                type="radio"
-                value="b"
-                name="design-color"
-              />
-              <ul className="section__design--palette">
-                <li className="box color3-1"></li>
-                <li className="box color3-2"></li>
-                <li className="box color3-3"></li>
-              </ul>
-            </label>
+            <Palettes />
           </div>
         </section>
       </>
