13baa6024ba8b246bbed7e78e0e47ca57dbb27c4
silviaespanagil <silviaespanagil@gmail.com>
2021-06-04 12:17:24 +0200

    Add collapsible function in design.js

--- react-proyect/src/components/Design.js
+++ react-proyect/src/components/Design.js
@@ -3,11 +3,29 @@ import Palettes from "./Palettes.js";
 import "../stylesheets/App.scss";
 
 class Design extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { className: "collapsible--close" };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    console.log("hice clic");
+    this.setState((prevState) => {
+      let changeClass =
+        prevState.className === "collapsible--close"
+          ? "collapsible--open"
+          : "collapsible--close";
+      return { className: changeClass };
+    });
+  }
   render() {
     return (
       <>
         <section className="section__design">
-          <div className="section__design--contain js-collapsible-header">
+          <div
+            className="section__design--contain js-collapsible-header"
+            onClick={this.handleClick}
+          >
             <h2 className="section__design--title">
               <i className="far fa-object-ungroup icon--design"></i>
               diseña
@@ -16,8 +34,11 @@ class Design extends React.Component {
               <i className="fa fa-chevron-down js-arrow"></i>
             </span>
           </div>
-          <div className="section__design--box js-collapsible collapsible--close">
+          <div
+            className={`section__design--box js-collapsible ${this.state.className}`}
+          >
             <h3 className="section__design--subtitle">Colores</h3>
+
             <Palettes />
           </div>
         </section>
