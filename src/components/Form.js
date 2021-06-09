9530150a4ee5b6a1b3beed7869515a114c39b9d9
silviaespanagil <silviaespanagil@gmail.com>
2021-06-09 12:35:22 +0200

    Change form class to function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Design from "./Design.js";
 import Fill from "./Fill.js";
 import Share from "./Share.js";
 import "../stylesheets/App.scss";
 
-class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { className: "collapsible--close" };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
+function Form(props) {
+  const [className, setClassName] = useState("collapsible--close");
+
+  function handleClick(ev) {
     console.log("hice clic");
-    this.setState((prevState) => {
+    setClassName((prevState) => {
       let changeClass =
         prevState.className === "collapsible--close"
           ? "collapsible--open"
@@ -20,19 +17,20 @@ class Form extends React.Component {
       return { className: changeClass };
     });
   }
-  render() {
-    return (
-      <>
-        <section className="sectionForm">
-          <form className="main-cards__form">
-            <Design /*handleClick={this.handleClick} state={this.state} */ />
-            <Fill />
-            <Share />
-          </form>
-        </section>
-      </>
-    );
-  }
+
+  return (
+    <>
+      <section className="sectionForm">
+        <form className="main-cards__form">
+          <Design /*handleClick={this.handleClick} state={this.state} */ />
+          <Fill />
+          <Share />
+        </form>
+      </section>
+    </>
+  );
+}
+{
 }
 
 export default Form;
