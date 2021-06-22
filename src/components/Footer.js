cce1f4404715d0b3e7273ce2cdc5bf0efb8614e5
Yosiris Mariñez <yosiris0510@gmail.com>
2021-06-22 14:22:47 +0200

    changes link card

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
     return (
       <>
         <footer className="footer">
-          <div className="footer__copy">Vegan Debuggers &copy 2021</div>
+          <div className="footer__copy">Vegan Debuggers &copy; 2021</div>
           <img className="footer__image" src={logo} alt="logo-adalab" />
         </footer>
       </>
