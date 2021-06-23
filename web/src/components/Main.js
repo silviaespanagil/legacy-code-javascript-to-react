5aee91695dcc78b636c2921e9bc3bc2d304a287f
silviaespanagil <silviaespanagil@gmail.com>
2021-06-23 18:29:10 +0200

    Start ls

--- web/src/components/Main.js
+++ web/src/components/Main.js
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "./Form.js";
 import CardPreview from "./PreviewCard.js";
+import ls from "../services/LocalStorage";
 
 function Main() {
-  let [paletteValue, setPalette] = useState("g");
-  let [nameValue, setName] = useState("");
-  let [jobValue, setJob] = useState("");
-  let [mailValue, setMail] = useState("");
-  let [phoneValue, setPhone] = useState("");
-  let [liValue, setLi] = useState("www.linkedin.com");
-  let [githubValue, setGithub] = useState("www.github.com");
-  const [image, setImage] = useState("");
+  const lsData = ls.get("data") || {};
+
+  let [paletteValue, setPalette] = useState(lsData.palette || "g");
+  let [nameValue, setName] = useState(lsData.name || "");
+  let [jobValue, setJob] = useState(lsData.job || "");
+  let [mailValue, setMail] = useState(lsData.email || "");
+  let [phoneValue, setPhone] = useState(lsData.phone || "");
+  let [liValue, setLi] = useState(lsData.linkedin || "www.linkedin.com");
+  let [githubValue, setGithub] = useState(lsData.github || "www.github.com");
+  const [image, setImage] = useState(lsData.photo || "");
 
   let handlePalette = (ev) => {
     setPalette(ev.currentTarget.value);
@@ -40,6 +43,10 @@ function Main() {
     setImage(avatar);
   };
 
+  /*useEffect(() => {
+    ls.set("data", data);
+  }, [data]);
+*/
   const handleButton = () => {
     setName("");
     setJob("");
--- web/src/services/LocalStorage.js
+++ web/src/services/LocalStorage.js
@@ -1,5 +1,12 @@
-// const get = () => {};
+const get = (key) => {
+  const data = localStorage.getItem(key);
+  return JSON.parse(data);
+};
 
-// const set = () => {};
+const set = (key, dataValue) => {
+  localStorage.setItem(key, JSON.stringify(dataValue));
+};
 
-// const LocalStorage = { get: get, set: set };
+const LocalStorage = { get: get, set: set };
+
+export default LocalStorage;
